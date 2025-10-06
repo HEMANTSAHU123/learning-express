8101bf82a693d6fa1d90eec2da0caec43ca4f628
HEMANTSAHU123 <hs1282542@gmail.com>
2025-10-06 10:46:57 +0530

    middle taught

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
-import http from 'http';
+
 import express from 'express'
 const app=express()
 
+let port=4000
 
 app.use((req,res,next)=>{
-    console.log('output from middleware 1')
+    console.log('Authentication middle called')
     next()
 })
-app.use((req,res,next)=>{
-    console.log('output from middleware 2')
-    res.send('<h1>Hello world</h1>')
+app.use("/library-2",(req,res,next)=>{
+    console.log('books recommondation from library!')
+    next();
 })
 
-app.listen(3001)
+app.use((req,res,next)=>{
+    console.log('special access to research paper from professors and seniors')
+    next()
+})
+app.get("/library-2",(req,res,next)=>{
+    res.send("<h1>Library two entered</h1>")
+})
+
+app.get("/library-3",(req,res,next)=>{
+    res.send("<h2>library 3 entered</h2>")
+    
+})
+app.listen(port,()=>{
+    console.log('server is running')
+})
