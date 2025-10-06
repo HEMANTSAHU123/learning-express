1336ce57cb49e6ad4258e5b703aa5539b75be976
HEMANTSAHU123 <hs1282542@gmail.com>
2025-10-06 11:50:48 +0530

    unknown routes

--- app.js
+++ app.js
@@ -5,20 +5,21 @@ const app=express()
 let port=4000
 
 app.use(express.json());
-
-app.get('/order',(req,res)=>{
-    res.send('here is the list of orders')
+app.get('/products',(req,res)=>{
+    res.send("Here is the list of all products.")
 })
-app.post("sorder/",(req,res)=>{
-res.send('A new order has been created')
+app.post("/products",(req,res)=>{
+    res.send("A new product has been added")
+})
+app.get('/categories',(req,res)=>{
+    res.send("here is the list of all categories")
+})
+app.post('/categories',(req,res)=>{
+    res.send("A new category has been created")
 })
 
-app.get('/users',(req,res)=>{
-    res.send('here is the list of all users')
-})
-
-app.post('/users',()=>{
-    res.send("A new user has been added.")
+app.use((req,res,next)=>{
+    res.status(404).send(`<h1>404-page not found</h1>`)
 })
 app.listen(port,()=>{
     console.log(`server is running at http://localhost:${port}`);
